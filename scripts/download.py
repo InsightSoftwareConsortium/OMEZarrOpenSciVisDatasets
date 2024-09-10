d2979588e184dab9d2a2300a15f5e6dc31122131
Matt McCormick <matt@mmmccormick.com>
2024-09-10 14:34:40 -0400

    Download nrrd header

--- scripts/download.py
+++ scripts/download.py
@@ -34,10 +34,10 @@ for dataset_name in datasets:
     size_bytes = size_xyz[0] * size_xyz[1] * size_xyz[2] * component_size
     if size_bytes < max_size:
         url = dataset["url"]
-        output_file = data_dir / "/".join(url.split("/")[-2:])
-        print(f"Downloading {dataset['name']} to {output_file}...")
-        output_file.parent.mkdir(parents=True, exist_ok=True)
 
+        output_file = data_dir / "/".join(url.split("/")[-2:])
+        print(f"Downloading {dataset_name} to {output_file}...")
+        output_file.parent.mkdir(parents=True, exist_ok=True)
         if output_file.exists():
             print(f"File {output_file} already exists, skipping download.")
         else:
@@ -45,6 +45,7 @@ for dataset_name in datasets:
                 with open(output_file, "wb") as f:
                     for chunk in response.iter_bytes():
                         f.write(chunk)
+
         sha512sum = dataset["sha512sum"]
         print(f"Verifying {output_file}...")
         with open(output_file, "rb") as f:
@@ -54,3 +55,14 @@ for dataset_name in datasets:
             output_file.unlink()
         else:
             print(f"Success: {output_file} passed checksum verification.")
+
+        nrrd_header = data_dir / ("/".join(url.split("/")[-2:-1])) / f"{dataset_name}.nhdr"
+        print(f"Downloading {dataset_name} NRRD header to {nrrd_header}...")
+        if nrrd_header.exists():
+            print(f"File {nrrd_header} already exists, skipping download.")
+        else:
+            nrrd_url = "/".join(url.split("/")[:-1]) + f"/{dataset_name}.nhdr"
+            with httpx.stream("GET", nrrd_url) as response:
+                with open(nrrd_header, "wb") as f:
+                    for chunk in response.iter_bytes():
+                        f.write(chunk)
