87d94b541998a9e59d1191c11c62b0545cd1a211
Matthew McCormick <matt@mmmccormick.com>
2025-05-25 16:41:34 -0400

    Use chunks x chunks_per_shard in output dir

--- scripts/gen-ome-zarrs.sh
+++ scripts/gen-ome-zarrs.sh
@@ -3,12 +3,19 @@
 set -e
 
 version=0.4
+chunks=64
+chunks_per_shard=0
 
 input_dir="data/open-scivis-datasets"
-output_dir="data/ome-zarr-open-scivis-datasets/v${version}"
+output_dir="data/ome-zarr-open-scivis-datasets/v${version}/${chunks}x${chunks_per_shard}"
 datasets_json="data/open-scivis-datasets/datasets.json"
 
 mkdir -p $output_dir
+
+chunks_per_shard_args=()
+if [ "$chunks_per_shard" -gt 1 ]; then
+  chunks_per_shard_args=("--chunks-per-shard" "$chunks_per_shard")
+fi
 
 # Iterate over all *.nhdr files in the data/open-scivis-datasets/**/*.nhdr directory
 # and run the `ngff-zarr` cli on the files to output the OME-Zarr for mat in the directory
@@ -20,6 +27,7 @@ for f in ${input_dir}/**/*.nhdr; do
     --method dask_image_gaussian \
     --input-backend itk \
     --chunks 64 \
+    ${chunks_per_shard_args[@]} \
     --name $name \
     -i $f \
     -o ${output_dir}/${name}.ome.zarr
